6aae76030dea646db28f93c9e95033449b6bdb81
Jaber4770 <jaber.ahmed4770@gmail.com>
2026-09-17 08:20:58 +0200

    nav done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,9 @@ import { NavLink } from "react-router";
 export default function Navbar() {
     return <>
         <div className="flex justify-between gap-5 bg-blue-900 font-bold text-white text-2xl py-5">
+            <div>
+                <NavLink className="hover:text-red-500 px-4 transition-all" to="/">JKR-Cinema</NavLink>
+            </div>
             <div className="flex gap-5">
                 <NavLink className="hover:text-red-500 px-4 transition-all" to="/">Home</NavLink>
                 <NavLink className="hover:text-red-500 px-4 transition-all" to="/movieSearch">Search Movie</NavLink>
