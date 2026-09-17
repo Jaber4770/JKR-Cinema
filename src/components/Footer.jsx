e40a897833cb482e8ce2167af43fa6061ca8b166
Jaber4770 <jaber.ahmed4770@gmail.com>
2026-09-17 08:12:55 +0200

    footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,12 @@
+import { NavLink } from "react-router";
 
 export default function Footer() {
   return <>
         <div className="h-16 bg-blue-900 text-white flex justify-center">
             <ul className="flex justify-center items-center font-bold gap-6 px-5 ">
-                <li>JKR-CinemaDB</li>
-                <li>Github</li>
-                <li>LinkedIn</li>
+                <NavLink to="/"><li>JKR-CinemaDB</li></NavLink>
+                <NavLink to="https://github.com/Jaber4770/JKR-Cinema"><li>Github</li></NavLink>
+                <NavLink to="https://it.linkedin.com/in/jaber4770"><li>LinkedIn</li></NavLink>
             </ul>
         </div>
     </>
